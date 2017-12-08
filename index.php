06af8dd29df3b97356239cbd105a0f1cc453b963
Ave KorveNoorkoiv <ave.korve-noorkoiv@khk.ee>
2017-12-08 09:27:42 +0200

    Kirjeldatud kommentaaride kasutamine

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ave
+ * Date: 08.12.2017
+ * Time: 9:03
+ */
+
+// mingi jama mis ei sobi - kommenteeritakse kahe //
+# see on üherealine kommentaar
+// üherealine kommentaar
+/**
+ * mitme
+ * realine kommentaar
+ *
+ */
+
+?>
+
