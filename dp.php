a5a38a0dd05579f359371cb969d8cdfe5da6f532
Ave KorveNoorkoiv <ave.korve-noorkoiv@khk.ee>
2018-01-22 16:38:43 +0200

    Andmebaasiga ühendamine

--- dp.php
+++ dp.php
@@ -5,10 +5,14 @@
  * Date: 22.01.2018
  * Time: 16:35
  */
+// n6uame andmebaasitootluse funkstioonid kasutusele
 require_once 'db_fnk.php';
+// tekitame yhendust andmebaasiga
 $dbYhendus = yhendus();
+// koostame sql lause ja saadame andmebaasi
 $sql = 'SHOW TABLES';
 $tulemus = annaAndmed($dbYhendus, $sql);
+// uurime tulemuse sisu test kujul
 echo '<pre>';
 print_r($tulemus);
 echo '</pre>';
