95f70fe13bbe4d691ca00ae9d10521cc0493a7d5
Ave KorveNoorkoiv <ave.korve-noorkoiv@khk.ee>
2018-01-22 16:26:18 +0200

    lihtkirje väljastamine

---
+++ mina.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ave
+ * Date: 22.01.2018
+ * Time: 16:18
+ */
+define('MINU_NIMI', 'Ave');
+echo 'Minu nimi on '.MINU_NIMI.'<br />';
+
+$minu_nimi = "Ave";
+print "Minu nimi on $minu_nimi";
+
+
