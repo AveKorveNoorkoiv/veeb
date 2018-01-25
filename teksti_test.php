ce201a784ac8d257ba9327d19ac2c9f069d4b898
Ave KorveNoorkoiv <ave.korve-noorkoiv@khk.ee>
2018-01-25 11:23:26 +0200

    Värvilise teksti konstruktor

--- teksti_test.php
+++ teksti_test.php
@@ -17,3 +17,23 @@ print_r($minuTekst);
 echo '</pre>';
 // prindime välja
 $minuTekst->prindiTekst();
+
+// loome veel ühe objekti
+// ja määrame sisu
+
+$mustTekst = new tekst('Must tekst');
+echo '<pre>';
+print_r($mustTekst);
+echo '</pre>';
+//prindime välja
+$mustTekst->prindiTekst();
+
+// nõuame kirjelduse faili
+require_once 'vtekst.php';
+//loome värvilise objekti
+//määrame sisu
+$punaneTekst = new vtekst('Punane tekst', '#FF0000');
+echo '<pre>';
+print_r($punaneTekst);
+echo '</pre>';
+$punaneTekst->prindiTekst();
