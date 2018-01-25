9f61be93da469f5e8b73e7e16b411f2a9393fd8d
Ave KorveNoorkoiv <ave.korve-noorkoiv@khk.ee>
2018-01-25 10:56:33 +0200

    Loodud tekstiobjekt ja kontrollitud tema töö

---
+++ teksti_test.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ave
+ * Date: 25.01.2018
+ * Time: 10:47
+ */
+require_once 'tekst.php';
+
+//loome teksti objeksti
+$minuTekst = new tekst();
+// määrame tekstile konkreetse susu
+$minuTekst->maaraTekst('Tere Maailm!');
+// vaatame, mis on objekti sees
+echo '<pre>';
+print_r($minuTekst);
+echo '</pre>';
+// prindime välja
+$minuTekst->prindiTekst();
