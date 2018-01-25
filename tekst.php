b093998a0efd06217da7f235e114ba292154dd2c
Ave KorveNoorkoiv <ave.korve-noorkoiv@khk.ee>
2018-01-25 10:46:05 +0200

    Teksti töötluse käivitus

---
+++ tekst.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ave
+ * Date: 25.01.2018
+ * Time: 10:32
+ */
+
+class tekst
+{ // classi algus
+    // classi omadused-väljad-fields
+    // klassi muutuja
+    var $sone = ''; // teksti kirjeldavad sõnad
+    // klassi tegevused
+    // meetoditena
+    //teksti määramine
+    // maaraTekst(Tere Maailm!);
+    function maaraTekst($sone) {
+        $this->sone = $sone;
+    }
+    //teksit väljastamine
+    function prindiTekst () {
+        echo $this->sone.'<br/>';
+    }
+} // classi lõpp
